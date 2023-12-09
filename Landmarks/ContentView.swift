b44a2135febda9e201028a0de06bf5a60c89d631
peder <peder@korsveien.me>
2023-12-09 10:12:04 +0100

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Peder Korsveien on 09/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+
+                    Spacer()
+
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Peder Korsveien on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
