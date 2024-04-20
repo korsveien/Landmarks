ea3d3ca636edad0402678474325002588cc9a50d
Peder Korsveien <peder.korsveien@bekk.no>
2024-04-20 08:43:45 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Peder Korsveien on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
